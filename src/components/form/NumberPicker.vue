0252ab2e340e7a9989e6e9508ea5057818d061fb
andre.bonfatti <andre.bonfatti@movile.com>
2017-05-21 21:51:30 -0300

    adding form components

---
+++ src/components/form/NumberPicker.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div>
+    <span class="control">+</span>
+    <span class="value">{{ number }}</span>
+    <span class="control">-</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['increment', 'value'],
+}
+</script>
+
+<style lang="css">
+</style>
